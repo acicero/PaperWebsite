7357d9410d6f5f0d2d44be984c6d492658003b16
Pidgeon Squish <pidgeonsquish@Kara.(none)>
2014-04-24 18:49:35 -0500

    Made more changes

--- cs435_Code/Author.php
+++ cs435_Code/Author.php
@@ -5,12 +5,15 @@ $profpic = "images/sign-up-btn.gif";
 // Create connection
 
 $con=mysqli_connect("localhost","root","","Paper_Website");
-
 // Check connection
-
 if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+$paper_selected = 0;
+if (isset($_POST['view_comments'])) {
+    $paper_selected = $_POST['papers'];
+    unset($_POST['view_comments']);    
   }
 ?>
 <head>
@@ -27,7 +30,7 @@ border:1px solid black;
 <body>
 	<div class = "AuthorDiv" style="float: left;">
 		<div class = "AuthorDiv2" style="float: left; bottom: 10px">
-			<form>
+			<form action="" method="post">
 				<table class="table" style="position:relative; width:100%;">
 					<th COLSPAN="4">
 						<h3><br>Your Posted</h3>
@@ -38,8 +41,7 @@ border:1px solid black;
 						<td width="15%">Status</td>
 						<td width="10%">Selected</td>					
 					<?php $result = mysqli_query($con,"SELECT * FROM `Paper` WHERE `userid` = 12345");
-					      $counter = 0;
-						while($row = mysqli_fetch_array($result)){ ?>
+					      while($row = mysqli_fetch_array($result)){ ?>
 						  <tr>						
   							<td><?php echo $row['title']; ?></td>
 							<td><?php echo $row['date']; ?></td>		
@@ -54,15 +56,15 @@ border:1px solid black;
 									  echo "pending";
 									} ?> </td>
 							<td>							
-								<input name= "papers" id = <?php echo "radio" . $counter; ?> type="radio"><br>							
+								<input name= "papers" value= <?php echo $row['paperid']; ?> type="radio"><br>						
 							</td>
 						  </tr>
- 						 <?php 
-							$counter++;		
-							} ?>				
-			<!-------------------------------------------------------------------------------->	
+ 						 <?php 									
+							} ?>										
 				</table>
+				<input type = 'submit' name = 'view_comments' value = "View Reviews">
 			</form>
+			<!-------------------------------------------------------------------------------->
 			<label style="position:relative; left:35%"> Submit a Paper Below </label>
 			<form style="position:relative; left:30%" name="input" action="" method="get">
 				Paper Title: <input type="text" name="user">
@@ -89,21 +91,24 @@ border:1px solid black;
 					<th width="70%">Comment</th>
 				</tr>
 				<?php 
+					if($paper_selected != 0){
+
 					$reviewers[] = array();
 					$count = 0;
-					$getReviewers = mysqli_query($con, "SELECT `paperid`,`reviewers_assigned1`,`reviewers_assigned2` FROM `Paper` WHERE `paperid` = 11111");
+					$getReviewers = mysqli_query($con, "SELECT `paperid`,`reviewers_assigned1`,`reviewers_assigned2` FROM `Paper` WHERE `paperid` = " . $paper_selected);
 					while($row = mysqli_fetch_array($getReviewers)){
 						$reviewers[0] = $row['reviewers_assigned1'];
 						$reviewers[1] = $row['reviewers_assigned2'];
 					}				
-					$review = mysqli_query($con,"SELECT * FROM `Reviews`,`User` WHERE `Reviews`.`written_to` = 12345 and `Reviews`.`paperid` = 11111 and ((`Reviews`.`userid` = " . $reviewers[0] . " and `User`.`userid` = " . $reviewers[0] . ") or (`Reviews`.`userid` = " . $reviewers[1] . " and `User`.`userid` = " . 								       $reviewers[1] . "))"); 
+					$review = mysqli_query($con,"SELECT * FROM `Reviews`,`User` WHERE `Reviews`.`written_to` = 12345 and `Reviews`.`paperid` = ". $paper_selected ." and ((`Reviews`.`userid` = " . $reviewers[0] . " and `User`.`userid` = " . $reviewers[0] . ") or (`Reviews`.`userid` = " . $reviewers[1] . " and `User`.`userid` = " . 								       $reviewers[1] . "))");   
 				        while($row = mysqli_fetch_array($review)){?>
 					  <tr>
 					    <td><?php echo $row['fname'] . " " . $row['lname'];?></td>
 					    <td><?php echo $row['rating'];?></td>
 					    <td><?php echo $row['review'];?></td>
 					  </tr>
-					<?php } ?> 		
+					<?php }
+					      } ?> 		
 				
 			</table>
 		</div>
