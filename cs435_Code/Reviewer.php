b9af03070de97b125532f11dbac305c40aa19644
Pidgeon Squish <pidgeonsquish@Kara.(none)>
2014-04-24 19:32:27 -0500

    Fixed the stuff with this

--- cs435_Code/Reviewer.php
+++ cs435_Code/Reviewer.php
@@ -12,6 +12,24 @@ if (mysqli_connect_errno())
   {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
+
+$paper_selected_download = 0;
+$paper_selected_review = 0;
+$paper_selected_comments = 0;
+if (isset($_POST['download'])) {
+    $paper_selected_download = $_POST['papers'];
+    unset($_POST['download']);    
+  }
+if (isset($_POST['Review'])) {
+    $paper_selected_review = $_POST['papers'];
+    unset($_POST['Review']);    
+  }
+if (isset($_POST['comments'])) {
+    $paper_selected_comments = $_POST['papers'];
+    unset($_POST['comments']);    
+  }
+
+
 ?>
 <head>
 <link href="style.css" rel="stylesheet" type="text/css" />
@@ -27,6 +45,7 @@ border:1px solid black;
 <body>
 	<div class = "AuthorDiv" style="float: left;">
 		<div class = "AuthorDiv2" style="float: left; bottom: 10px">
+			<form action="" method="post">
 			<table class="tablesmall" style="position:relative; width:100%;">
 				<th COLSPAN="4">
 					<h3><br>Awaiting Review</h3>
@@ -35,23 +54,22 @@ border:1px solid black;
 					<td width="60%">Title</td>
 					<td width="15%">Date</td>					
 					<td width="10%">Selected</td>
-				<?php  $result = mysqli_query($con,"SELECT * FROM (SELECT * FROM `Paper` WHERE `Paper`.`paperid` NOT IN(SELECT `paperid` FROM `Reviews`)) tab WHERE ".							      "((tab.`reviewers_assigned1` = 12346) or(tab.`reviewers_assigned2` = 12346))");
+				<?php  $result = mysqli_query($con,"SELECT * FROM (SELECT * FROM `Paper` WHERE `Paper`.`paperid` NOT IN(SELECT `paperid` FROM `Reviews`)) tab WHERE ".							      "((tab.`reviewers_assigned1` = 12346) or(tab.`reviewers_assigned2` = 12346))");//this needs the usersession id
 				while($row = mysqli_fetch_array($result)){ ?>
 					  <tr>
 					    <td><?php echo $row['title'];?></td>
 					    <td><?php echo $row['date'];?></td>
-					    <td>
-						<form style="font-size:20px; position:relative; left: 13px; top: 8px;">
-							<input type="radio"><br>
-							</form>
+					    <td>						
+							<input type="radio" name="papers" value=<?php echo $row['paperid']; ?>><br>							
 						</td>
 					  </tr>
 					<?php } ?>
 				
 			</table>
-			<form style="position:relative; bottom: -10px; text-align:center" name="input" action="">
 				<input type="submit" value="Download">
-			</form>
+				<input type="submit" value="Review">
+			</form>	
+			<form action="" method="post">			
 			<table class="tablesmall" style="position:relative; width:100%; bottom: -10px">
 				<th COLSPAN="4">
 					<h3><br>Completed Review</h3>
@@ -62,7 +80,7 @@ border:1px solid black;
 					<td width="15%">Status</td>
 					<td width="10%">Selected</td>
 				<?php  
-					$result2 = mysqli_query($con,"SELECT * FROM `Reviews`,`Paper` WHERE `Reviews`.`userid` = 12346 and `Reviews`.`paperid` = `Paper`.`paperid` and "								."((`Paper`.`reviewers_assigned1` = 12346) or(`Paper`.`reviewers_assigned2` = 12346))");
+					$result2 = mysqli_query($con,"SELECT * FROM `Reviews`,`Paper` WHERE `Reviews`.`userid` = 12346 and `Reviews`.`paperid` = `Paper`.`paperid` and "								."((`Paper`.`reviewers_assigned1` = 12346) or(`Paper`.`reviewers_assigned2` = 12346))");//this also needs the usersession id
 
 					while($row = mysqli_fetch_array($result2)){ ?>
 					  <tr>
@@ -78,28 +96,29 @@ border:1px solid black;
 								default:
 								  echo "pending";
 								} ?> </td>
-						<td>
-						<form style="font-size:20px; position:relative; left: 13px; top: 8px;">
-							<input type="radio"><br>
-							</form>
-						</td>
+					    <td>						
+						<input type="radio" name="papers" value=<?php echo $row['paperid']; ?>><br>							
+					    </td>
 					  </tr>
-					<?php } ?>
-			
+					<?php } ?>			
 			</table>
-		</div>
-		<div class="AuthorDiv2" style="float: left; left:2px; bottom: 10px">
-
-			<form style="position:relative; left:30%; bottom: -10px;" name="input" action="" method="get">
-				Rating: 1 <input type="radio"> 2<input type="radio"> 3<input type="radio"> 4<input type="radio"> 5<input type="radio">
-			</form>
-			Review to Author: </br>
-			<textarea rows="15" cols="72">Insert shit you say to the fucking Author here</textarea></br>
-			Comment to Editor: </br>
-			<textarea rows="15" cols="72">Here's where you spew shit onto a text feild to the dickhead Editor</textarea></br>
-			<form style="position:relative; bottom: -10px; text-align:center" name="input" action="">
-				<input type="submit" value="Submit Review">
+			<input type="submit" value="View Comment" name='comments'>
 			</form>
 		</div>
+		<?php  
+			
+			$result2 = mysqli_query($con,"SELECT * FROM `Reviews` WHERE `Reviews`.`userid` = 12346 and `Reviews`.`paperid` = " . $paper_selected_comments);//this also needs the usersession id
+			$review_row = mysqli_fetch_array($result2); ?>
+		<form name="input" action="" method="get"><!-- Dis where for sends to php file-->
+			<div class="AuthorDiv2" style="float: left; left:2px; bottom: 10px">			
+				Rating: 1 <input type="radio"<?php if($review_row['rating'] == 1){echo "checked";}?> name="rating"> 2<input type="radio"<?php if($review_row['rating'] == 2){echo "checked";}?>  name="rating"> 3<input type="radio"<?php if($review_row['rating'] == 3){echo "checked";}?>  name="rating"> 4<input type="radio"<?php if($review_row['rating'] == 4){echo "checked";}?>  name="rating"> 5<input type="radio"<?php if($review_row['rating'] == 5){echo "checked";}?>  name="rating">			
+				Review to Author: </br>
+				<textarea rows="15" cols="72"><?php echo $review_row['review'];?></textarea></br>
+				Comment to Editor: </br>
+				<textarea rows="15" cols="72"><?php echo $review_row['comment'];?></textarea></br>			
+				<input type="submit" value="Submit Review">			
+			</div>
+		</form>
+		
 	</div>
 </body>
